dc6208bde098c27d9117d5dccb7c934afbb9b3f3
michael <mikhail.v.rogachev@gmail.com>
2024-04-20 14:20:50 +0500

    second commit

---
+++ app/src/main/java/com/mikhail/lesson1/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.mikhail.lesson1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+   private lateinit var tx1 : TextView
+   private lateinit var bt1 : Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        tx1 = findViewById(R.id.tx_1)
+        bt1 = findViewById(R.id.btColor)
+
+        bt1.setOnClickListener{
+            tx1.text = getText(R.string.By)
+        }
+    }
+}
