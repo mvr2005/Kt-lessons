59c218629fe780a79e65a6315abab41f56116d61
michael <mikhail.v.rogachev@gmail.com>
2024-04-20 14:50:55 +0500

    MainActivity2 plus

--- app/src/main/java/com/mikhail/lesson1/MainActivity2.kt
+++ app/src/main/java/com/mikhail/lesson1/MainActivity2.kt
@@ -1,37 +1,20 @@
 package com.mikhail.lesson1
 
-import android.opengl.Visibility
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-   private lateinit var tx1 : TextView
-   private lateinit var bt1 : Button
-
-
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        tx1 = findViewById(R.id.tx_1)
-        bt1 = findViewById(R.id.btColor)
-
-        bt1.setOnClickListener{
-            tx1.text = getText(R.string.By)
-            bt1.visibility = View.GONE
         }
     }
 }
