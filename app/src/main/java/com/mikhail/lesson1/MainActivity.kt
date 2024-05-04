e587005afd5015dba5f871d72e9404e5b1cd29fa
michael <mikhail.v.rogachev@gmail.com>
2024-05-04 15:19:59 +0500

    commit04.05.24

--- app/src/main/java/com/mikhail/lesson1/MainActivity.kt
+++ app/src/main/java/com/mikhail/lesson1/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.mikhail.lesson1
 
-import android.opengl.Visibility
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
+import android.util.Log
 import android.view.View
 import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.ColorRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -36,27 +39,42 @@ class MainActivity : AppCompatActivity() {
             tx1.text = getText(R.string.By)
             bt1.visibility = View.GONE
             bt2.visibility = VISIBLE
+            val textSecond = "oooooo"
+            val num = "F".toCharArray()
+            Log.d("MyLog", "bt $textSecond")
+            Log.d("MyLog", "f $num")
         }
+       Log.d("MyLog", "onCreate")
     }
 
     override fun onStart() {
         super.onStart()
+        Log.d("MyLog","onStart")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d("MyLog","onResume")
     }
 
     override fun onPause() {
         super.onPause()
+        Log.d("MyLog","onPause")
     }
 
     override fun onStop() {
         super.onStop()
+        Log.d("MyLog","onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d("MyLog","onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MyLog","onRestart")
     }
 
 }
